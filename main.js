9014d8beabd3047016819222685125cb71071212
BeetrootAcademyAccount2 <beetroot444@yahoo.com>
2024-03-30 15:03:24 +0200

    Add code

---
+++ main.js
@@ -0,0 +1,99 @@
+//Find the area of a square and a circle inside of it
+
+/*
+let a = 6;
+const pi = 3.14;
+const squareArea = a * a;
+let biggerSquareSide = a + 2;
+debugger
+const biggerSquareArea = biggerSquareSide * biggerSquareSide;
+let r = a / 2;
+pi = 3.13;
+const circleArea = r * r * pi;
+console.log(circleArea);
+
+let myName = "Ivo";
+let myFamilyName = `Nikolov`;
+
+let fullName = `${myName} ${myFamilyName}`; // TEMPLATE LITERAL
+let fullNameAlternative = myName + " " + myFamilyName;
+
+// console.log(myName);
+// console.log(myFamilyName);
+console.log(fullNameAlternative);
+
+let age = 32;
+
+let isDayMonday = false;
+
+let person = {
+  myName: myName,
+  age: age,
+  isStudent: false,
+};
+
+let symbol = Symbol("symbol");
+let symbol2 = Symbol("symbol");
+
+let emptyValue;
+
+console.log(symbol === symbol2);
+
+console.log(typeof emptyValue);
+
+let bigInt = 3n;
+
+let maxValue = Number.MIN_SAFE_INTEGER;
+
+console.log(maxValue);
+
+const a = "Ivo";
+const b = 6;
+const sum = a + b;
+console.log(sum);
+console.log(typeof sum);
+*/
+
+/*
+const a = "3.23";
+
+//const convertNumber = Number(a);
+//const convertNumber = parseInt(a);
+//const convertNumber = parseFloat(a);
+const convertNumber = +a;
+
+console.log(convertNumber);
+console.log(typeof convertNumber);
+*/
+/*
+const a = 55.7666;
+
+//const convertString = String(a);
+// const convertString = a.toString();
+//const convertString = a.toFixed(1);
+const convertString = a + "";
+
+console.log(convertString);
+console.log(typeof convertString);
+*/
+
+/*
+const a = 5;
+const b = 2;
+
+const result = b ** a;
+console.log(result);
+
+const area = a * r ** 2;
+
+*/
+
+let a = "Ivo";
+const b = 5;
+
+// console.log("AND: ", a && b);
+// console.log("OR: ", a || b);
+// console.log("!A: ", a);
+// console.log("!B: ", !!b);
+
+console.log(a > b);
